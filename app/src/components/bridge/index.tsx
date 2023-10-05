bfc7a754007f912b4f024481f4d3cf90e0e4e89a
d-roak <me+git@droak.sh>
2023-10-05 20:25:00 +0900

    add interface for PoC bridging

--- app/src/components/bridge/index.tsx
+++ app/src/components/bridge/index.tsx
@@ -1,24 +1,115 @@
+import { Button, OutlinedInput, Select } from "@mui/material";
+import { useState } from "react";
+import { useAccount, useBalance } from "wagmi";
 
 export default function Bridge() {
+  let balance = Number(useBalance({
+    address: useAccount().address
+  }).data?.formatted)
+  let eth_balance = isNaN(balance) ? 0 : balance.toFixed(5)
+  let [amount, setAmount] = useState(0)
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log(amount)
+  }
+
   return (
-    <div className="w-96 bg-[#f0f0f0] rounded-lg">
-      <div className="p-4">
-        <div className="flex flex-col items-center justify-center">
+    <div className="p-1 w-[30vw] rounded-lg bg-gradient-to-br from-[#AB679F] to-[#FB9489]">
+      <form
+        className="p-4 w-full bg-[#f0f0f0] rounded-lg"
+        onSubmit={handleSubmit}>
+        <div className="mt-2">
+          Token:
+          <Select
+            disabled
+            className="w-1/5 mt-4 ml-3"
+            variant="outlined"
+            native
+            value="eth"
+            inputProps={{
+              name: 'token',
+              id: 'outlined-token-native-simple',
+            }}
+          >
+            <option value="eth">ETH</option>
+          </Select>
+        </div>
+        <div className="mt-2">
           <div className="text-2xl font-bold">
             From:
           </div>
-          Network - amount - token
-          (Balance: available (clickable to fill))
+          <div className="mt-5 flex flex-row items-center gap-2">
+            <Select
+              disabled
+              className="w-1/3"
+              variant="outlined"
+              native
+              value="ethereum"
+              inputProps={{
+                name: 'network',
+                id: 'outlined-network-native-simple',
+              }}
+            >
+              <option value="ethereum">Ethereum</option>
+              <option value="starknet">Starknet</option>
+            </Select>
+            
+            <OutlinedInput
+              className="w-2/3"
+              placeholder="Amount"
+              type="number"
+              endAdornment="ETH"
+              value={amount}
+              onChange={(e) => setAmount(Number(e.target.value))}
+            />
+          </div>
+
+          (Balance: <span onClick={() => setAmount(Number(eth_balance))} className="cursor-pointer text-[#48A6B2]">{eth_balance} ETH</span>)
         </div>
 
-        <div className="flex flex-col items-center justify-center mt-8">
+        <div className="mt-8">
           <div className="text-2xl font-bold">
             To (estimated):
           </div>
-          Network - amount - token
+
+          <div className="mt-5 flex flex-row items-center gap-2">
+            <Select
+              disabled
+              className="w-1/3"
+              variant="outlined"
+              native
+              value="starknet"
+              inputProps={{
+                name: 'network',
+                id: 'outlined-network-native-simple',
+              }}
+            >
+              <option value="ethereum">Ethereum</option>
+              <option value="starknet">Starknet</option>
+            </Select>
+            
+            <OutlinedInput
+              disabled
+              className="w-2/3"
+              placeholder="Amount"
+              type="number"
+              endAdornment="yabETH"
+              value={amount}
+            />
+          </div>
         </div>
 
-      </div>
+        <div className="mt-8">
+          <Button
+            variant="contained"
+            className="w-full py-2 px-4 bg-[#48A6B2] hover:bg-[#FB9489] text-white"
+            type="submit"
+          >
+            Bridge
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
--- app/src/components/wallet/starknet/disconnect-modal.tsx
+++ app/src/components/wallet/starknet/disconnect-modal.tsx
@@ -2,12 +2,12 @@
 import React from "react";
 
 import { useAccount, useDisconnect } from "@starknet-react/core";
-import { Button, Dialog, DialogContent, DialogTitle } from "@mui/material";
+import { Box, Button, Modal, Typography } from "@mui/material";
 
 export default function DisconnectModal() {
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
-  const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [modalOpen, setModalOpen] = React.useState(false);
 
   const addressShort = address
     ? `${address.slice(0, 6)}...${address.slice(-4)}`
@@ -15,16 +15,23 @@ export default function DisconnectModal() {
 
   return (
     <div className="w-full flex justify-end">
-      <Button onClick={() => setDialogOpen(true)}>{addressShort}</Button>
-      <Dialog
-        open={dialogOpen}>
-        <DialogContent>
-          <DialogTitle>Disconnect Wallet</DialogTitle>
-          <div className="flex flex-col gap-4">
-            <Button onClick={() => { disconnect(); setDialogOpen(false)}}>Disconnect</Button>
+      <Button onClick={() => setModalOpen(true)}>{addressShort}</Button>
+      <Modal
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[400px] max-w-[90vw] bg-gradient-to-r from-[#48A6B2] to-[#FB9489] p-1 rounded-lg">
+          <div className="bg-[#1D1A35] text-white py-10 px-2 rounded-lg">
+            <Typography id="modal-modal-title" variant="h6" component="h2" className="text-center">
+              Disconnect Wallet
+            </Typography>
+            <div id="modal-modal-description" className="flex flex-col gap-4">
+              <Button onClick={() => { disconnect(); setModalOpen(false)}}>Disconnect</Button>
+            </div>
           </div>
-        </DialogContent>
-      </Dialog>
+        </Box>
+      </Modal>
     </div>
   );
 }

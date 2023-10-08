1464821380b2db6ec6ef2f9fa8fbd0d57aa700e5
d-roak <me+git@droak.sh>
2023-10-09 03:30:29 +0900

    comment on herodotus api key

--- app/src/utils/constants.tsx
+++ app/src/utils/constants.tsx
@@ -4,6 +4,7 @@ export const starknet_bridge_addr = "0x0000000"
 
 export const herodotus_api_endpoint = "https://api.herodotus.cloud/"
 //export const herodotus_api_key = process.env.HERODOTUS_API_KEY as string
+// no problem being public (it was already in another public repo)
 export const herodotus_api_key = "c0a1de24-9f68-42f7-a424-ba20008373cb"
 
 export const available_networks = [

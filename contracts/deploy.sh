479b9d9a53aa7e9bbefd938b624f80c9533f516a
d-roak <me+git@droak.sh>
2023-10-12 16:08:53 +0900

    base dockerfile for deploying the contracts and docker-compose with env vars

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd solidity
-ETH_CONTRACT_ADDRESS=`forge script scripts/Deploy.s.sol:Deploy --rpc-url goerli --broadcast | grep "Contract Address:" | awk '{print $3}'`
+cd /contracts/solidity
 
-echo "ETH_CONTRACT_ADDRESS=$ETH_CONTRACT_ADDRESS"
+#echo `PRIVATE_KEY="$ETH_PRIV_KEY"` > .env
+#echo `GOERLI_RPC_URL="$ETH_GOERLI_RPC_URL"` >> .env
+#echo `ETHERSCAN_API_KEY="$ETHERSCAN_API_KEY"` >> .env
+
+ETH_BRIDGE_ADDRESS=`~/.foundry/bin/forge script script/Deploy.s.sol:Deploy --rpc-url goerli --broadcast | grep "Contract Address:" | awk '{print $3}'`
+
+echo "ETH_BRIDGE_ADDRESS=$ETH_BRIDGE_ADDRESS"
+echo "ETH_BRIDGE_ADDRESS=$ETH_BRIDGE_ADDRESS" >> /contracts/app/.env
